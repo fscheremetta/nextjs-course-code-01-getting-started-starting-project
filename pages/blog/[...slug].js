394650a33e83f518b27bf3f28fa97dd93d96a0d2
Fernanda Scheremetta <fernandascheremetta@gmail.com>
2023-11-01 11:36:39 -0300

    catch all routes

--- pages/blog/[...slug].js
+++ pages/blog/[...slug].js
@@ -4,6 +4,7 @@ function BlogPostsPage() {
   const router = useRouter();
 
   console.log(router.query);
+  //return an array of all segments slugs
   return (
     <div>
       <h1>The Blog Posts</h1>
